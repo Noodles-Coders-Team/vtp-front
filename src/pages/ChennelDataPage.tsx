567b7f65ec7e300fdee93d0481d59570ac1618fa
geggabyte <kolesnikov.genka@gmail.com>
2026-07-23 17:35:46 +0200

    Added slider to detemine the range

--- src/pages/ChennelDataPage.tsx
+++ src/pages/ChennelDataPage.tsx
@@ -3,50 +3,77 @@ import Card from "@/components/commonComponents/Card";
 import { useEffect, useState } from "react";
 import { readChannelData } from "@/api/ChannelDataApi";
 import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import { Column, Container, Row } from "@/components/commonComponents/Container";
 
-
-const DaysLimit: number = 30;
 
 
 export default function ChannelData() {
     const [channelData, setChannelData] = useState<ChannelDataDto[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
+    const [daysLimit, setDaysLimit] = useState<number>(30);
 
-    const loadData = async () => {
+    const loadData = async (update: boolean = true) => {
         try {
-            setLoading(true);
+            if (update)
+                setLoading(true);
 
             const data = await readChannelData();
-            setChannelData(data.slice(-DaysLimit));
+            if (daysLimit == 0) {
+                setChannelData(data);
+            }
+            else {
+                //TODO: this should be at the API level
+                setChannelData(data.slice(-daysLimit));
+            }
         } catch (err) {
             setError((err as Error).message);
         } finally {
-            setLoading(false);
+            if (update)
+                setLoading(false);
         };
     };
 
-    useEffect(() => {
-        loadData();
-    }, []);
+    useEffect(() => { loadData() }, []);
+
+    useEffect(() => { loadData(false) }, [daysLimit]);
 
     if (loading) return <p>Loading channel data...</p>;
 
     if (error) return <p style={{ color: "red" }}>Error loading data: {error}</p>;
 
     return (
-        <Card title="Channel Data!!!">
-            <AreaChart
-                height={400}
-                width={500}
-                data={channelData}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis<ChannelDataDto, string> dataKey="id" />
-                <YAxis<ChannelDataDto, number> />
-                <Tooltip />
-                <Area<ChannelDataDto, number> type="monotone" dataKey="views" stroke="#3aca23" fill="#83c01b" />
-            </AreaChart>
+        <Card title="Channel Data graph!">
+            <Container>
+                <Row>
+                    <Column>
+                        <label className="form-label">Limit to last: {daysLimit} days</label>
+                    </Column>
+                </Row>
+                <Row>
+                    <Column>
+                        <input type="range" className="form-range" min="0" max="100" step="5" id="range"
+                            style={{ width: '20%' }}
+                            value={daysLimit}
+                            onChange={(e) => { e.preventDefault(); setDaysLimit(Number(e.target.value)); }}></input>
+                    </Column>
+                </Row>
+                <Row>
+                    <Column>
+                        <AreaChart
+                            height={400}
+                            width={500}
+                            data={channelData}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis<ChannelDataDto, string> dataKey="id" />
+                            <YAxis<ChannelDataDto, number> />
+                            <Tooltip />
+                            <Area<ChannelDataDto, number> type="monotone" dataKey="views" stroke="#3aca23" fill="#83c01b" />
+                        </AreaChart>
+                    </Column>
+                </Row>
+            </Container>
         </Card>
     )
 }
